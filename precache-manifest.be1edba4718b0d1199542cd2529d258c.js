a3839e5ccf8ed4912507fadb1910995f33eaa0b8
wes-santos <weslleyalmerindo2@gmail.com>
2022-07-02 00:49:57 -0300

    Updates

---
+++ precache-manifest.be1edba4718b0d1199542cd2529d258c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565050245e2a669bbb2312806d80a0dc",
+    "url": "/starwars-plannet-search/index.html"
+  },
+  {
+    "revision": "6fcd6f4b59be652f75a0",
+    "url": "/starwars-plannet-search/static/css/main.afd64638.chunk.css"
+  },
+  {
+    "revision": "2b3335a88850d0e21809",
+    "url": "/starwars-plannet-search/static/js/2.9e3f1d8d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-plannet-search/static/js/2.9e3f1d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fcd6f4b59be652f75a0",
+    "url": "/starwars-plannet-search/static/js/main.316a8b79.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/starwars-plannet-search/static/js/main.316a8b79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6191bace561374cd397",
+    "url": "/starwars-plannet-search/static/js/runtime-main.f6fe46a4.js"
+  }
+]);
